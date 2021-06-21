59dea238afaeb27edb32d35cdff9be89c960ceb1
Ashutosh Saxena <ashutosh.saxena@roundassist.ca>
2021-06-21 10:24:36 +0530

    Updates

---
+++ precache-manifest.58b3862a948bd75cf9fb151f7d1cdcce.js
@@ -0,0 +1,93 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c49b6e7befe20b672ecd0a25000dd9e",
+    "url": "/ReactApps/index.html"
+  },
+  {
+    "revision": "35f031da0394957cf2b2",
+    "url": "/ReactApps/static/css/2.8e20672c.chunk.css"
+  },
+  {
+    "revision": "2a5ac3df00510b92f003",
+    "url": "/ReactApps/static/css/main.8adaf100.chunk.css"
+  },
+  {
+    "revision": "35f031da0394957cf2b2",
+    "url": "/ReactApps/static/js/2.38eb834d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApps/static/js/2.38eb834d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5ac3df00510b92f003",
+    "url": "/ReactApps/static/js/main.04eb9e3e.chunk.js"
+  },
+  {
+    "revision": "4bc299e72e8e720df3a1",
+    "url": "/ReactApps/static/js/runtime-main.87817e2f.js"
+  },
+  {
+    "revision": "26e4a70376548e5487f61a290b5164a1",
+    "url": "/ReactApps/static/media/CNLogo.26e4a703.svg"
+  },
+  {
+    "revision": "c201bb4ba9b10d6bc57e7c8bead58c3f",
+    "url": "/ReactApps/static/media/Calculater.c201bb4b.svg"
+  },
+  {
+    "revision": "d6f03dc6c5759ca16582069fa85f50ca",
+    "url": "/ReactApps/static/media/ChuckNorrisJokes.d6f03dc6.svg"
+  },
+  {
+    "revision": "5fb397b8c5425353bb3882940fe6466f",
+    "url": "/ReactApps/static/media/CodeEditor.5fb397b8.svg"
+  },
+  {
+    "revision": "62ec4210b88e158dc60576c1c988f4ff",
+    "url": "/ReactApps/static/media/FaceRecognition.62ec4210.svg"
+  },
+  {
+    "revision": "ea5ba632000f8a0c34226ddad96cd719",
+    "url": "/ReactApps/static/media/RoboFriends.ea5ba632.svg"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ReactApps/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6ea4df279840f0419a311f68d3d208b2",
+    "url": "/ReactApps/static/media/Titillium-Regular.6ea4df27.woff"
+  },
+  {
+    "url": "/ReactApps/static/media/c8a9c5064d8faa5e7acdecfeee4e9075.3007c6d2.woff"
+  },
+  {
+    "url": "/ReactApps/static/media/c8a9c5064d8faa5e7acdecfeee4e9075.3486fcf8.woff2"
+  },
+  {
+    "url": "/ReactApps/static/media/c8a9c5064d8faa5e7acdecfeee4e9075.62acc5aa.svg"
+  },
+  {
+    "url": "/ReactApps/static/media/c8a9c5064d8faa5e7acdecfeee4e9075.93a5345c.eot"
+  },
+  {
+    "url": "/ReactApps/static/media/c8a9c5064d8faa5e7acdecfeee4e9075.c8a9c506.ttf"
+  },
+  {
+    "revision": "1e5cee7c72c72fd33d9db53c9fbe8707",
+    "url": "/ReactApps/static/media/collapse.1e5cee7c.svg"
+  },
+  {
+    "revision": "779331cb232b3b7b7a193157b5cef0e4",
+    "url": "/ReactApps/static/media/download.779331cb.svg"
+  },
+  {
+    "revision": "cca05a65fbf9bdcedfcf968291de5f73",
+    "url": "/ReactApps/static/media/expand.cca05a65.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactApps/static/media/logo.5d5d9eef.svg"
+  }
+]);
